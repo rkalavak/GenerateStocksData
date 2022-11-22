a10950819c82795ad852643a95117f1c8ffd4c14
rkalavak <kalavakurirenu@gmail.com>
2022-11-22 06:41:34 +0530

    Dates modified

--- src/main/java/com/kalavakuri/generatestocksdata/HistoricalAverageRSIStockCalculatorJson.java
+++ src/main/java/com/kalavakuri/generatestocksdata/HistoricalAverageRSIStockCalculatorJson.java
@@ -14,7 +14,7 @@ import com.google.gson.Gson;
 public class HistoricalAverageRSIStockCalculatorJson {
 
 	private static final String MONEY_CONTROL_STOCK_URL = "https://priceapi.moneycontrol.com/pricefeed/nse/equitycash/";
-	private static final String NSE_HISTORY_URL = "https://www.nseindia.com/api/historical/cm/equity?symbol=&series=[%22EQ%22]&from=05-08-2022&to=05-11-2022";
+	private static final String NSE_HISTORY_URL = "https://www.nseindia.com/api/historical/cm/equity?symbol=&series=[%22EQ%22]&from=05-08-2022&to=22-11-2022";
 	private static final String REFERRER_URL = "https://www.nseindia.com/get-quotes/equity?symbol=";
 
 	public static void main(String[] args) throws Exception {
