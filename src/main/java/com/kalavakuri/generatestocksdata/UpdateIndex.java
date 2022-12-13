f0c4139dd5e4179f409ad861308479f31f9c3fc7
rkalavak <kalavakurirenu@gmail.com>
2022-12-13 06:22:00 +0530

    Date updated

--- src/main/java/com/kalavakuri/generatestocksdata/UpdateIndex.java
+++ src/main/java/com/kalavakuri/generatestocksdata/UpdateIndex.java
@@ -119,7 +119,7 @@ public class UpdateIndex {
 
 				HoldIndexDataVO holdIndexDataVO = entry.getValue();
 				statement.addBatch("INSERT INTO " + entry.getKey()
-						+ "(TRADE_DATE, OPEN_VALUE, HIGH_VALUE, LOW_VALUE, CLOSE_VALUE) VALUES(TO_DATE('09-12-2022'),"
+						+ "(TRADE_DATE, OPEN_VALUE, HIGH_VALUE, LOW_VALUE, CLOSE_VALUE) VALUES(TO_DATE('12-12-2022'),"
 						+ holdIndexDataVO.getOpenValue() + "," + holdIndexDataVO.getHighValue() + ","
 						+ holdIndexDataVO.getLowValue() + "," + holdIndexDataVO.getCloseValue() + ")");
 			}
